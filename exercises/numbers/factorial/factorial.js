217e2fcfddf26c9aad8e3387f014cf8ba1d51d77
Jesse Farmer <jesse@20bits.com>
2020-01-29 14:49:22 -0800

    First version of factorial

--- exercises/numbers/factorial/factorial.js
+++ exercises/numbers/factorial/factorial.js
@@ -23,7 +23,11 @@
  * @returns {number} The factorial of `n`
  */
 function factorial(n) {
-  // This is your job. :)
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
 
 if (require.main === module) {
